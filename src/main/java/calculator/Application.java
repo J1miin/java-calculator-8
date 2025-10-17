4d58295e47e3c087ed092a3c86f707cef3d9859f
Jimin <gift040404@naver.com>
2025-10-17 20:07:39 +0900

    feat(Application) : connect all the classes
    connect InputView, InputParser, Calculator, and OutputView

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,17 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        inputView.start();
+
+        InputParser inputParser = new InputParser();
+        inputParser.checkInput(inputView.getInput());
+        inputView.setNums(inputParser.getNums());
+
+        Calculator calculator = new Calculator();
+        calculator.calculate(inputView.getNums());
+
+        OutputView outputView = new OutputView();
+        outputView.printResult(calculator.getAnswer());
     }
 }
