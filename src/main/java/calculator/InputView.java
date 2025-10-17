0fae63147ad743d9dbf5a358c1018eb48031bd5f
Jimin <gift040404@naver.com>
2025-10-17 19:42:29 +0900

    feat(InputView) : add read user Input

--- src/main/java/calculator/InputView.java
+++ src/main/java/calculator/InputView.java
@@ -1,8 +1,21 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    private String sInput;
     InputView(){}
+
     public void start(){
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+        setInput();
+    }
+
+    public void setInput(){
+        this.sInput= Console.readLine();
+    }
+
+    public String getInput(){
+        return this.sInput;
     }
 }
