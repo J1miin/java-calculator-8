35135b8abc1daf2f9f55f87ed18801be60fae20c
Jimin <gift040404@naver.com>
2025-10-18 17:40:06 +0900

    fix(InputParser) : fix InputParser logics
    Fix InputParser to handle empty input and parse numbers correctly:
    - add isEmpty method to handle empty input
    - fix separateNum method to use String.split
    - change customDelimiter type to String to use String.split

--- src/main/java/calculator/InputParser.java
+++ src/main/java/calculator/InputParser.java
@@ -2,15 +2,15 @@ package calculator;
 
 import java.util.Vector;
 
-import static java.lang.Character.isDigit;
-
 public class InputParser {
     public static final String NOT_POSITIVE = "양수가 아닙니다.";
+    public static final String DELIMITER_LENGTH = "커스텀구분자가 1개보다 많습니다.";
     public static final String STRANGE_DELIMITER = "커스텀구분자가 이상합니다.";
     public static final String STRANGE_INPUT = "입력값이 이상합니다.";
+    public static final String EMPTY_INPUT = "입력값이 없습니다.";
 
     private int startIdx =0;
-    private char customDelimiter =' ';
+    private String customDelimiter ="";
     private Vector<Integer> nums;
 
     InputParser(){
@@ -18,43 +18,49 @@ public class InputParser {
     }
 
     public void checkInput(String sInput) {
+        isEmpty(sInput);
         findDelimiter(sInput);
         separateNum(sInput);
         checkPositive();
     }
 
+    public void isEmpty(String sInput) {
+        if (sInput.isEmpty()) {
+            exception(EMPTY_INPUT);
+        }
+    }
+
     public void findDelimiter(String sInput){
         if (sInput.charAt(0)=='/' && sInput.charAt(1)=='/'){
-            customDelimiter= sInput.charAt(2);
-            this.startIdx=5;
+            if (sInput.charAt(3)=='\\' && sInput.charAt(4)=='n'){
+                customDelimiter += sInput.charAt(2);
+                this.startIdx=5;
+            } else {
+                exception(DELIMITER_LENGTH);
+            }
         }
     }
 
     public void separateNum(String sInput){
-        String sTmp = "";
-        for (int i = this.startIdx; i<sInput.length(); i++){
-            char cTmp = sInput.charAt(i);
-            if (isDigit(cTmp)){
-                sTmp = sTmp + cTmp;
-            }else {
-                if (i== this.startIdx) exception(STRANGE_DELIMITER);
-                if (customDelimiter!=' '){
-                    if (cTmp==customDelimiter){
-                        if (!sTmp.isEmpty()) {
-                            nums.add(Integer.parseInt(sTmp));
-                        }
-                    }
-                }else{
-                    if (cTmp == ',' || cTmp==':') {
-                        nums.add(Integer.parseInt(sTmp));
-                    }else{
-                        exception(STRANGE_INPUT);
-                    }
-                }
-                sTmp = "";
+        String[] splitter ;
+        if (customDelimiter.length() == 1) {
+            sInput= sInput.substring(this.startIdx);
+            if (customDelimiter.equals(".")) {
+                splitter = sInput.split("\\.");
+            } else {
+                splitter = sInput.split(customDelimiter);
+            }
+        } else {
+            splitter = sInput.split("[,:]");
+        }
+
+        for (String s : splitter) {
+            try {
+                nums.add(Integer.parseInt(s));
+            } catch (NumberFormatException e) {
+                exception(STRANGE_INPUT);
             }
         }
-        nums.add(Integer.parseInt(sTmp));
     }
 
     private void checkPositive(){
@@ -64,8 +70,6 @@ public class InputParser {
                     exception(NOT_POSITIVE);
                 }
             }
-        }else {
-            exception(STRANGE_INPUT);
         }
     }
 
