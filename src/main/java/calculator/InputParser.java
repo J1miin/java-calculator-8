539f0c771f1cc7005f186195a7cacdae0dd374f6
Jimin <gift040404@naver.com>
2025-10-18 18:08:58 +0900

    refactor(InputParser) : refactor inputparser
    two has fixed :
    - renamed variable sInput to input
    - deleted unused constant STRANGE_DELIMITER

--- src/main/java/calculator/InputParser.java
+++ src/main/java/calculator/InputParser.java
@@ -5,35 +5,34 @@ import java.util.Vector;
 public class InputParser {
     public static final String NOT_POSITIVE = "양수가 아닙니다.";
     public static final String DELIMITER_LENGTH = "커스텀구분자가 1개보다 많습니다.";
-    public static final String STRANGE_DELIMITER = "커스텀구분자가 이상합니다.";
     public static final String STRANGE_INPUT = "입력값이 이상합니다.";
     public static final String EMPTY_INPUT = "입력값이 없습니다.";
 
     private int startIdx =0;
     private String customDelimiter ="";
-    private Vector<Integer> nums;
+    private final Vector<Integer> nums;
 
     InputParser(){
         this.nums = new Vector<>();
     }
 
-    public void checkInput(String sInput) {
-        isEmpty(sInput);
-        findDelimiter(sInput);
-        separateNum(sInput);
+    public void checkInput(String input) {
+        isEmpty(input);
+        findDelimiter(input);
+        separateNum(input);
         checkPositive();
     }
 
-    public void isEmpty(String sInput) {
-        if (sInput.isEmpty()) {
+    public void isEmpty(String input) {
+        if (input.isEmpty()) {
             exception(EMPTY_INPUT);
         }
     }
 
-    public void findDelimiter(String sInput){
-        if (sInput.charAt(0)=='/' && sInput.charAt(1)=='/'){
-            if (sInput.charAt(3)=='\\' && sInput.charAt(4)=='n'){
-                customDelimiter += sInput.charAt(2);
+    public void findDelimiter(String input){
+        if (input.charAt(0)=='/' && input.charAt(1)=='/'){
+            if (input.charAt(3)=='\\' && input.charAt(4)=='n'){
+                customDelimiter += input.charAt(2);
                 this.startIdx=5;
             } else {
                 exception(DELIMITER_LENGTH);
@@ -41,17 +40,17 @@ public class InputParser {
         }
     }
 
-    public void separateNum(String sInput){
+    public void separateNum(String input){
         String[] splitter ;
         if (customDelimiter.length() == 1) {
-            sInput= sInput.substring(this.startIdx);
+            input= input.substring(this.startIdx);
             if (customDelimiter.equals(".")) {
-                splitter = sInput.split("\\.");
+                splitter = input.split("\\.");
             } else {
-                splitter = sInput.split(customDelimiter);
+                splitter = input.split(customDelimiter);
             }
         } else {
-            splitter = sInput.split("[,:]");
+            splitter = input.split("[,:]");
         }
 
         for (String s : splitter) {
