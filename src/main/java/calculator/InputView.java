886d4fafd1cc06cf43cdb4d0897f1aa8d5fe3316
Jimin <gift040404@naver.com>
2025-10-20 12:55:27 +0900

    refactor(InputView) : delete data storage
    - remove vNum field and related methods

--- src/main/java/calculator/InputView.java
+++ src/main/java/calculator/InputView.java
@@ -2,31 +2,22 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
-
 public class InputView {
     private String sInput;
-    private ArrayList<Integer> vNum;
-    InputView(){}
 
-    public void start(){
+    InputView() {
+    }
+
+    public void start() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         setInput();
     }
 
-    public void setInput(){
-        this.sInput= Console.readLine();
+    public void setInput() {
+        this.sInput = Console.readLine();
     }
 
-    public String getInput(){
+    public String getInput() {
         return this.sInput;
-    }
-
-    public void setNums(ArrayList<Integer> vNum){
-        this.vNum = vNum;
-    }
-
-    public ArrayList<Integer> getNums(){
-        return this.vNum;
     }
 }
